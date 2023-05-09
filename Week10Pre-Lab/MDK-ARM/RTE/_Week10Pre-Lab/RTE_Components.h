3ae00b49b14fa4170b3b415cc1614d9e08b08d76
Yusuf <yusufbilgin1905@gmail.com>
2023-05-09 21:56:04 +0300

    Week10

---
+++ Week10Pre-Lab/MDK-ARM/RTE/_Week10Pre-Lab/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Week10Pre-Lab' 
+ * Target:  'Week10Pre-Lab' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
